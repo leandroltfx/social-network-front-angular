9c57b51d40c865cff2285faa8132d3f9c5f43fea
leandroltfx <leandrobsb55@hotmail.com>
2024-03-02 19:56:24 -0300

    criando testes unitários para o login

--- src/app/features/login/login.component.spec.ts
+++ src/app/features/login/login.component.spec.ts
@@ -1,4 +1,12 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +16,17 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule,
+      ]
     })
     .compileComponents();
 
@@ -19,5 +37,43 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('login', () => {
+
+    const spyLog = spyOn(console, 'log');
+
+    component.loginForm = component.buildLoginForm();
+    component.loginForm.controls['email'].setValue('email@email.com');
+    component.loginForm.controls['password'].setValue('password123');
+    component.login();
+    expect(spyLog).toHaveBeenCalled();
+  });
+
+  it('login - error email', () => {
+
+    component.loginForm = component.buildLoginForm();
+    component.loginForm.controls['email'].setValue('');
+    component.loginForm.controls['password'].setValue('password123');
+    component.login();
+    expect(component.loginForm.invalid).toBeTrue();
+  });
+
+  it('login - error password', () => {
+
+    component.loginForm = component.buildLoginForm();
+    component.loginForm.controls['email'].setValue('email@email.com');
+    component.loginForm.controls['password'].setValue('');
+    component.login();
+    expect(component.loginForm.invalid).toBeTrue();
+  });
+
+  it('login - both fields', () => {
+
+    component.loginForm = component.buildLoginForm();
+    component.loginForm.controls['email'].setValue('');
+    component.loginForm.controls['password'].setValue('');
+    component.login();
+    expect(component.loginForm.invalid).toBeTrue();
   });
 });
