a26228f9a83e45be4c840a369332aa5a7f06f8c3
leandroltfx <leandrobsb55@hotmail.com>
2024-03-05 16:41:30 -0300

    alterações de nome do projeto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/free-market-front-angular'),
+      dir: require('path').join(__dirname, './coverage/social-network-front-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
