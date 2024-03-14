ee15a65c07540815e01879d22dba36cf68854375
leandroltfx <leandrobsb55@hotmail.com>
2024-03-14 17:08:20 -0300

    criando layout para feature de resetar a senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
   { path: 'user-registration', loadChildren: () => import('./features/user-registration/user-registration.module').then(m => m.UserRegistrationModule) },
+  { path: 'forgot-password', loadChildren: () => import('./features/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) },
   { path: '**', redirectTo: 'login' }
 ];
 
